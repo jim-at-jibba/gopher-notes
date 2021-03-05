99bc40bff23663a67771489dbabd17bf4535d587
James Best <jim@justjibba.net>
2021-03-05 04:22:05 +0000

    Update migration

--- pkg/repository/migrations/000001_create_users_table.up.sql
+++ pkg/repository/migrations/000001_create_users_table.up.sql
@@ -2,5 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY,
     created_at      timestamp with time zone default now() not null,
     updated_at      timestamp with time zone,
-    name text not null
+    username text not null,
+    password text not null
 );
